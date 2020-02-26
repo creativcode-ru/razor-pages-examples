b1703c45973767b2ef01092f742a3ea002c267f9
Алексей <burdykin@live.ru>
2020-02-26 18:26:49 +0300

    Обновление страниц

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations.Schema; //для изменения схемы БД
+
 namespace RazorPagesMovie.Models
 {
     public class Movie
@@ -11,9 +13,12 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; } //Поле ID является обязательным для первичного ключа базы данных.
         public string Title { get; set; }
 
+        [Display(Name = "Дата выхода")]
         [DataType(DataType.Date)] //Атрибут DataType указывает тип данных (Date). Отображается только дата, не требуется вводить сведения о времени в поле даты
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")] //изменение схемы колонки в БД
         public decimal Price { get; set; }
     }
 }
